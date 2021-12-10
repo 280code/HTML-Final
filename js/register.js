6fd0c803359b4c0042f9ac8e1e11dd84836fe227
YfNightWind <lyh1147557099@outlook.com>
2021-12-11 01:35:33 +0800

    实现未同意协议就无法点击注册的功能

--- js/register.js
+++ js/register.js
@@ -1,5 +1,17 @@
 function isChecked() {
-    var result = document.getElementsByName("isChecked")[0].checked;
-    console.log(result);
-    return result;
+    var isChecked = document.getElementsByName("isChecked")[0].checked;
+    if (isChecked) {
+        document.getElementById("fill").disabled = false;
+    } else {
+        document.getElementById("fill").disabled = true;
+    }
+
 }
+
+// if (isChecked == 1) {
+//     var button = document.getElementById('fill');
+//     button.disabled = false;
+// } else {
+//     var button = document.getElementById('fill');
+//     button.disabled = true;
+// }
