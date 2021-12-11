5b03596e81a0eb7a843ce98c83eb4cc6c70f8875
YfNightWind <lyh1147557099@outlook.com>
2021-12-11 20:54:04 +0800

    ✅初步完成注册，细节有待打磨

--- js/register.js
+++ js/register.js
@@ -1,17 +1,73 @@
-function isChecked() {
+function checkeAgreement() {
     var isChecked = document.getElementsByName("isChecked")[0].checked;
     if (isChecked) {
         document.getElementById("fill").disabled = false;
     } else {
         document.getElementById("fill").disabled = true;
     }
-
 }
 
-// if (isChecked == 1) {
-//     var button = document.getElementById('fill');
-//     button.disabled = false;
-// } else {
-//     var button = document.getElementById('fill');
-//     button.disabled = true;
-// }
+function isEmail(e) {
+    var atindex = e.indexOf("@");
+    var dotindex = e.indexOf(".", atindex);
+    var flag = true;
+    subStr = e.substring(0, dotindex - 1);
+
+    if (
+        atindex < 1 ||
+        atindex != e.lastIndexOf("@") ||
+        dotindex < atindex + 2 ||
+        e.length <= subStr.length
+    ) {
+        flag = false;
+    } else {
+        flag = true;
+    }
+    return flag;
+}
+
+function checkCommits() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var rPassword = document.getElementById("r-password");
+    var email = document.getElementById("email");
+
+    // name validation
+    if (username.value == "") {
+        alert("用户名不能为空！");
+        return false;
+    }
+
+    // password validation
+    if (password.value != "" || rPassword.value != "") {
+        if (password.value.length < 6 || password.value.length > 16) {
+            alert("密码长度应在6-16范围内！");
+            return false;
+        } else if (password.value != rPassword.value) {
+            alert("两次密码输入不一致，请重新输入！");
+            return false;
+        }
+    } else {
+        alert("密码不能为空！");
+        return false;
+    }
+
+    // email validation
+    if (email.value != "") {
+        //验证email格式
+        if (!isEmail(email.value)) {
+            alert("请输入正确的邮箱！");
+            return false;
+        }
+    } else {
+        alert("邮箱不能为空！");
+        return false;
+    }
+
+    // stroe in localstorage
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("password", password.value);
+    localStorage.setItem("email", email.value);
+
+    window.location.href = "login.html";
+}
