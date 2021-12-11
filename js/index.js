d76b92addae65b2259eaa85f65a77129b4a61bca
280code <63572275+280code@users.noreply.github.com>
2021-12-12 03:40:49 +0800

    适应学校所有分辨率

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ nav.classList.add("add_header");
 nav.width = window.innerWidth;
 div.style.display = "none";
 let video = document.querySelector("video");
+document.documentElement.scrollTop = 0;
 video.width = window.innerWidth;
+if (window.innerHeight < 626) {
+    video.height = 700;
+}
+
+
 document.addEventListener("mousewheel", (e) => {
     if (e.wheelDelta > 0) {
         console.log("鼠标滚轮向上滚动。");
