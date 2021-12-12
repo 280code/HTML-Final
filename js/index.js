eb884a09efb8f3b73f45546ce449b21e77a3d6af
280code <63572275+280code@users.noreply.github.com>
2021-12-12 20:00:22 +0800

    给首页增加了一下事件
    鼠标滚轮向下滑动切换视频

--- js/index.js
+++ js/index.js
@@ -7,13 +7,15 @@ video.width = window.innerWidth;
 if (window.innerHeight < 626) {
     video.height = 700;
 }
-
-
+let srcs = ["./resource/video/wonderful_tools.mp4", "./resource/video/sec3-video.mp4"]
+let i = 0;
 document.addEventListener("mousewheel", (e) => {
     if (e.wheelDelta > 0) {
         console.log("鼠标滚轮向上滚动。");
 
     } else if (e.wheelDelta < 0) {
         console.log("鼠标滚轮向下滚动。")
+        video.src = srcs[++i % 2];
+
     }
 })
