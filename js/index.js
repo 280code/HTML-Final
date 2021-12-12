07702b305527a96c065d336268f6ba3c92cfb23d
280code <63572275+280code@users.noreply.github.com>
2021-12-12 20:59:40 +0800

    更改了视频源

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ video.width = window.innerWidth;
 if (window.innerHeight < 626) {
     video.height = 700;
 }
-let srcs = ["./resource/video/wonderful_tools.mp4", "./resource/video/sec3-video.mp4"]
+let srcs = ["./resource/video/wonderful_tools.mp4", "./resource/video/sec3-video.mp4", "./resource/video/xiaomik40.mp4"]
 let i = 0;
 document.addEventListener("mousewheel", (e) => {
     if (e.wheelDelta > 0) {
@@ -15,7 +15,6 @@ document.addEventListener("mousewheel", (e) => {
 
     } else if (e.wheelDelta < 0) {
         console.log("鼠标滚轮向下滚动。")
-        video.src = srcs[++i % 2];
-
+        video.src = srcs[++i % 3];
     }
 })
