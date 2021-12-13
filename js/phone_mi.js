a489ed3dc03bdf13fad654568b5ed9ab1ee8a996
280code <63572275+280code@users.noreply.github.com>
2021-12-13 22:01:18 +0800

    更改了规划和小米的页面以及规范

--- js/phone_mi.js
+++ js/phone_mi.js
@@ -39,3 +39,55 @@ function push() {
         requestAnimationFrame(push);
     }
 }
+//商品数据
+class commodity {
+    constructor(src, title, price) {
+        this.src = src;
+        this.title = title;
+        this.price = price;
+    }
+
+}
+var miPhone = [];
+for (let i = 0; i < 7; i++) {
+    miPhone[i] =
+        new commodity(`./resource/mi_images/px2-${i+1}.webp`, "小米", 12)
+}
+
+/*DOM部分*/
+
+
+
+let mainRight = document.createElement("div");
+mainRight.id = "main-right";
+let html = '';
+let lenght = miPhone.length;
+for (let i = 0; i < 4; i++) {
+    let item = miPhone[1];
+    html += `<div id="picgroup">
+            <a href="./details.html">
+                <div class="cloth3"><img src="resource/mi_images/px2-1.webp"><br><span id="title">Redmi 10x 4G</span> <br><span id="price">￥3333.00</span></div>
+            </a>
+            <a href="./details.html">
+                <div class="cloth3"><img src="resource/mi_images/px2-1.webp"><br><span id="title">Redmi 10x 4G</span> <br><span id="price">￥3333.00</span></div>
+            </a>
+            <a href="./details.html">
+                <div class="cloth3"><img src="resource/mi_images/px2-1.webp"><br><span id="title">Redmi 10x 4G</span> <br><span id="price">￥3333.00</span></div>
+            </a>
+        </div>`
+}
+
+mainRight.innerHTML = html;
+document.body.insertAdjacentElement("beforeend", mainRight);
+
+let demo2 = document.querySelector("#demo1");
+console.log(demo2)
+demo2.addEventListener("click", () => {
+    alert("hello");
+    localStorage.del = "hello";
+})
+
+function demo(i) {
+    localStorage.demo = i;
+    return false;
+}
