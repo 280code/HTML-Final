1de22501aa540838097beb711d3ca4c46f61a7bb
00fiary <1991713106@qq.com>
2021-12-17 22:13:28 +0800

    完善了宝贝和失效宝贝

---
+++ js/favorites_main.js
@@ -0,0 +1,31 @@
+let favorites_main = document.createElement("div");
+favorites_main.id = "cloth-love";
+let html = '';
+let lenght = main.length;
+for (let i = 0; i < 4; i++) {
+    let item = main[1];
+    html += `<div class="picture">
+
+                <div class="text">
+                    <a href="">
+                        <img src="resource\favorites\delete-01.jpg" alt="">
+                    </a>
+                    <div class="left">
+                        <a href="">
+                                    进入店铺
+                                    </a>
+                    </div>
+                    <div class="right">
+                        <a href="">
+                                找相似
+                            </a>
+                    </div>
+
+                </div>
+                <img src="resource\phone\fail-03.png">
+            </div>
+
+            <p>vivo<br>
+                <p><span>¥</span>5500.00</p>
+            </p>`
+}
