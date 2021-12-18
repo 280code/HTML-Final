61f882f158cb5c712b56dede6e93b4918f2f28d8
280code <63572275+280code@users.noreply.github.com>
2021-12-18 19:02:57 +0800

    增加收藏夹的重复增加元素代码

--- js/favorites_main.js
+++ js/favorites_main.js
@@ -1,14 +1,14 @@
 let favorites_main = document.createElement("div");
 favorites_main.id = "cloth-love";
+//以id为main-allBaby的元素作为插入元素的父元素
+let main = document.querySelector("#main-allBaby");
 let html = '';
-let lenght = main.length;
 for (let i = 0; i < 4; i++) {
-    let item = main[1];
-    html += `<div class="picture">
-
+    html += `        <div class="cloth-love">
+            <div class="picture">
                 <div class="text">
                     <a href="">
-                        <img src="resource\favorites\delete-01.jpg" alt="">
+                        <img src="resource/favorites/delete-01.jpg" alt="">
                     </a>
                     <div class="left">
                         <a href="">
@@ -22,10 +22,13 @@ for (let i = 0; i < 4; i++) {
                     </div>
 
                 </div>
-                <img src="resource\phone\fail-03.png">
+                <img src="resource/phone/fail-03.png">
             </div>
-
             <p>vivo<br>
                 <p><span>¥</span>5500.00</p>
-            </p>`
+            </p>
+        </div>`
+
 }
+//在父元素的最后面插入元素
+main.insertAdjacentHTML("beforeend", html);
