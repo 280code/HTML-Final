df0346371660e72c34d998c6472f1e0206e68f58
00fiary <1991713106@qq.com>
2021-12-20 00:18:43 +0800

    失效宝贝删除商品

--- js/favorites_failure.js
+++ js/favorites_failure.js
@@ -1,3 +1,21 @@
+//切换空状态和有商品状态
+let i = true;
+let empty = document.querySelector(".empty");
+let allBaby = document.querySelector("#main-allBaby");
+//let promptd = document.querySelector(".delete_tip");
+//切换空状态和有商品状态
+function toSwitch() {
+    if (i) {
+        empty.style.display = "block";
+        allBaby.style.display = "none";
+        i = false;
+    } else {
+        empty.style.display = "none";
+        allBaby.style.display = "block";
+        i = true;
+    }
+}
+
 let favorites_main = document.createElement("div");
 favorites_main.id = "cloth-love";
 //以id为main-allBaby的元素作为插入元素的父元素
@@ -10,7 +28,7 @@ for (let i = 0; i < 4; i++) {
                     <div class="question">确定删除？</div>
                     <div class="select">
                         <a href="#" class="yes">确定</a>
-                        <a href="#" onclick="no1();" class="no">取消</a>
+                        <a href="#"  class="no nol">取消</a>
                     </div>
                 </div>
 
@@ -19,7 +37,7 @@ for (let i = 0; i < 4; i++) {
             <div class="picture">
                 <div class="text">
 
-                    <a href="#" onclick="delete1();">
+                    <a href="#" class="delete1">
                         <img src="resource/favorites/delete-01.jpg" alt="">
                     </a>
                     <div class="left">
@@ -46,24 +64,8 @@ for (let i = 0; i < 4; i++) {
 }
 //在父元素的最后面插入元素
 main.insertAdjacentHTML("beforeend", html);
-//切换空状态和有商品状态
-let i = true;
-let empty = document.querySelector(".empty");
-let allBaby = document.querySelector("#main-allBaby");
-//let promptd = document.querySelector(".delete_tip");
-//切换空状态和有商品状态
-function toSwitch() {
-    if (i) {
-        empty.style.display = "block";
-        allBaby.style.display = "none";
-        i = false;
-    } else {
-        empty.style.display = "none";
-        allBaby.style.display = "block";
-        i = true;
-    }
-}
 
+//选择是否删除商品页面
 function delete1() {
     if (document.querySelector('.delete').style.display = 'none') {
         document.querySelector('.delete').style.display = 'block';
@@ -77,3 +79,23 @@ function no1() {
     }
 
 }
+//遍历
+function init() {
+    let clothLoves = document.querySelectorAll(".cloth-love");
+    //遍历每个收藏
+    clothLoves.forEach((clothLoves) => {
+        //获取垃圾桶图标
+        let delete1 = clothLoves.querySelector(".delete1");
+        //给每个垃圾桶绑定事件
+        delete1.addEventListener("click", () => {
+            clothLoves.querySelector(".delete").style.display = "block";
+
+        })
+        let nol = clothLoves.querySelector(".nol");
+        //给取消按钮事件绑定点击事件
+        nol.addEventListener("click", () => {
+            clothLoves.querySelector(".delete").style.display = "none";
+        })
+    })
+}
+init();
